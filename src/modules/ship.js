25e3147af8568dcbabf06f55b9fe644296fea627
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2025-11-22 18:12:29 +0300

    feat: implement Ship class with hit tracking and sinking logic

---
+++ src/modules/ship.js
@@ -0,0 +1,31 @@
+export default class Ship {
+	#length;
+	#numHits;
+	#isSunk;
+	constructor(length) {
+		this.#length = length;
+		this.#numHits = 0;
+		this.#isSunk = false;
+	}
+
+	get length() {
+		return this.#length;
+	}
+
+	get numHits() {
+		return this.#numHits;
+	}
+
+	get isSunk() {
+		return this.#isSunk;
+	}
+
+	#checkSunk() {
+		return this.#numHits >= this.#length;
+	}
+
+	hitShip() {
+		this.#numHits++;
+		this.#isSunk = this.#checkSunk();
+	}
+}
---
+++ src/tests/ship.test.js
@@ -0,0 +1,39 @@
+import Ship from "../modules/ship.js";
+
+describe("ship module tests.", () => {
+	let ship;
+	beforeAll(() => {
+		ship = new Ship(5);
+	});
+	test("ship module exists", () => {
+		expect(Ship).toBeDefined();
+	});
+	test("ship module should include length property with correct value", () => {
+		expect(ship.length).toBeDefined();
+		expect(ship.length).toBe(5);
+	});
+	test("ship module should include a a property that shows the number of times it has been hit that starts with 0", () => {
+		expect(ship.numHits).toBeDefined();
+		expect(ship.numHits).toBe(0);
+	});
+	test("ship module should include a flag property that indicates whether the ship is sunk or not and starts with false", () => {
+		expect(ship.isSunk).toBe(false);
+	});
+	test("ship module should include a method to hit the ship", () => {
+		expect(ship.hitShip).toBeDefined();
+		expect(ship.hitShip).toBeInstanceOf(Function);
+	});
+	test("hitShip method should increment the numHits property by 1", () => {
+		ship.hitShip();
+		expect(ship.numHits).toBe(1);
+	});
+	test("hitShip should sink the ship if it the number of times hit is greater than or equal to the length of the ship", () => {
+		// Hit the ship four times so that the ship gets hit 5 times in total
+		for (let i = 0; i < 4; i++) {
+			ship.hitShip();
+		}
+
+		expect(ship.numHits).toBe(5);
+		expect(ship.isSunk).toBe(true);
+	});
+});
