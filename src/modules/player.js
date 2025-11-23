46a168c6fbd81864593b1cabec2b4d753c5768b8
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2025-11-23 21:30:28 +0300

    feat: enhance Player class to include attacks array and add corresponding tests

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,13 +1,29 @@
 import GameBoard from "./gameboard.js";
 
 export class Player {
+	#boardSize;
 	constructor(name, boardSize) {
 		this.name = name;
-		this.gameBoard = new GameBoard(boardSize);
+		this.#boardSize = boardSize;
+		this.attacks = [];
+		this.gameBoard = new GameBoard(this.#boardSize);
+		this.#initializeAttacks();
 	}
 	makeAttack(player, x, y) {
 		const playerBoard = player.gameBoard;
+		const res = playerBoard.receiveAttack(x, y);
+		this.attacks[y][x] = res.ship ? "hit" : "noHit";
 		return playerBoard.receiveAttack(x, y);
+	}
+
+	#initializeAttacks() {
+		for (let i = 0; i < this.#boardSize; i++) {
+			const row = [];
+			for (let j = 0; j < this.#boardSize; j++) {
+				row.push("unknown");
+			}
+			this.attacks.push(row);
+		}
 	}
 }
 
