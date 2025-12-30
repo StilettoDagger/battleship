b368c9308c8cec93dffe3f4ed988fb84f360446d
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2025-12-30 09:16:53 +0300

    feat(queue): add clear method and improve documentation for queue methods

--- src/modules/queue.js
+++ src/modules/queue.js
@@ -4,10 +4,18 @@ export default class Queue {
 		this.head = 0;
 	}
 
+	/**
+	 * Add an item to the queue.
+	 * @param {*} v - The item to be added.
+	 */
 	enqueue(v) {
 		this.items.push(v);
 	}
 
+	/**
+	 * Remove an element from the head of the queue.
+	 * @returns the dequeued element.
+	 */
 	dequeue() {
 		if (this.isEmpty()) return undefined;
 
@@ -21,15 +29,35 @@ export default class Queue {
 		return v;
 	}
 
+	/**
+	 * Get the current head of the queue.
+	 * @returns the head of the queue.
+	 */
 	peek() {
 		return this.items[this.head];
 	}
 
+	/**
+	 * Checks if the queue is empty.
+	 * @returns true if it is empty and false otherwise.
+	 */
 	isEmpty() {
 		return this.head >= this.items.length;
 	}
 
+	/**
+	 * Gets the current size of the queue.
+	 * @returns the size of the queue.
+	 */
 	size() {
 		return this.items.length - this.head;
+	}
+
+	/**
+	 * Clears the queue.
+	 */
+	clear() {
+		this.items = [];
+		this.head = 0;
 	}
 }
